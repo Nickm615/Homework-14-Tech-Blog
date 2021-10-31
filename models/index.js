ac80bb9c7b1f63ad7912ecd7f14f9cca9f05535d
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-30 22:12:22 -0500

    Created Post model and relationships between User and Post

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
 const User = require('./User');
+const Post = require('./Post')
 
-module.exports = { User };
+Post.hasOne(User, {
+    foreignKey: 'creator',
+    onDelete: 'SET NULL'
+});
+
+User.belongsTo(Post, {
+    foreignKey: 'creator'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'creator',
+    onDelete: 'SET NULL'
+})
+
+
+module.exports = { User, Post };
